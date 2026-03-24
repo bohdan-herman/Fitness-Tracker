13cd82aab37586d01d0ce28355cd4c55ecc21064
lukker444 <zp.bogdan.osnova@gmail.com>
2026-03-24 09:02:53 +0100

    Fixing bugs with Prisma Client

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,5 +1,15 @@
-import pkg from "@prisma/client";
-const { PrismaClient } = pkg;
-const prisma = new PrismaClient();
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = process.env.DATABASE_URL;
+
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+  log: ["query", "info", "warn", "error"],
+});
 
 export default prisma;
